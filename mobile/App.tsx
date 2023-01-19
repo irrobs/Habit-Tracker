0c97d7038fbe9e3ac9450f70ab89d70acb309d25
Matheus Borri <matheus.escobar90@gmail.com>
2023-01-19 02:44:57 -0300

    import change to date pt-br on App.tsx

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -3,6 +3,7 @@ import { StatusBar } from 'react-native';
 import {useFonts, Inter_400Regular, Inter_600SemiBold, Inter_700Bold, Inter_800ExtraBold} from '@expo-google-fonts/inter' //importação da fonts para uso. É necessário instalar expo-fonts com: npx expo install expo-font @expo-google-fonts/nomeDaFonte 
 import {Loading} from './src/components/loading'
 import { Home } from './src/screens/Home';
+import './src/lib/dayjs';
 
 //criação e exportação do componente utilizando React Native. Componentes sempre começam com letra maiúscula. View e Text são as tagas para o mobile, funcionam como  as tags para web, por exemplo o View e como se fosse uma div, o Text como um p.
 export default function App() {
